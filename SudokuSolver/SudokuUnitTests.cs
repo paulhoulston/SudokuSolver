2b6a5275431befe2232aa37d3a71ee7a35ecc538
paulhoulston <paul.houlston@hotmail.co.uk>
2015-07-09 12:13:45 +0100

    Added first test for simplest 1x1 grid (comilation fails)

---
+++ SudokuSolver/SudokuUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace SudokuSolver
+{
+    public class Given_I_have_a_1_by_1_grid
+    {
+        public class When_the_grid_is_not_populated
+        {
+            [Test]
+            public void Then_the_sudoku_solver_enters_the_correct_value_for_me()
+            {
+
+                Assert.AreEqual(1, singleGridValue);
+            }
+        }
+    }
+}
